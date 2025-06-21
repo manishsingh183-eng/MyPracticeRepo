a307f65a7cf0b21f16b1b597d06a11c7886e0d30
manishsingh183-eng <18manishsingh.3@gmail.com>
2025-06-21 19:15:57 +0530

    dsa q7

---
+++ week1/Data structures and Algorithms/q7_FinancialForecasting/Code/q7_FinancialForecasting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class FinancialForecast
+{
+    static void Main(string[] args)
+    {
+        double presentValue = 10000; 
+        double growthRate = 0.08;    
+        int years = 5;               
+
+        Console.WriteLine("Financial Forecast using Recursion");
+        double futureValue = CalculateFutureValue(presentValue, growthRate, years);
+        Console.WriteLine($"Future Value after {years} years: ₹{futureValue:F2}");
+    }
+    static double CalculateFutureValue(double pv, double rate, int n)
+    {
+        if (n == 0)
+            return pv;
+
+        return (1 + rate) * CalculateFutureValue(pv, rate, n - 1);
+    }
+}
