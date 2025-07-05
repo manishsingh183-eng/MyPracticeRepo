e0251d7cd801a1504d7c9c6f33eb52a3f517eb97
manishsingh183-eng <18manishsingh.3@gmail.com>
2025-07-05 19:08:14 +0530

    Week 3 assignment

---
+++ week3/EF core/RetailInventory upto lab5/RetailInventory/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventory.Models;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=RetailStoreDB;Trusted_Connection=True;TrustServerCertificate=True;Encrypt=False;");
+    }
+}
